d2b6a632ffc59ce8149c050b116aa69f62d3b49e
akash-23git <uic.17bca1245@gmail.com>
2021-07-13 03:00:42 -0700

    Created sidebar menu

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Portfolio from "./components/portfolio/Portfolio";
 import Works from "./components/works/Works";
 import Testimonials from "./components/testimonials/Testimonials";
 import Contact from "./components/contact/Contact";
+import Menu from "./components/menu/Menu";
 import "./app.scss";
 import {useState} from "react";
 
@@ -13,6 +14,7 @@ function App() {
   return (
     <div className="app">
     <TopBar  menuOpen={menuOpen}  setMenuOpen={setMenuOpen}/>
+    <Menu menuOpen={menuOpen}  setMenuOpen={setMenuOpen}/>
     <div className="sections">
     <Intro />
     <Portfolio />
