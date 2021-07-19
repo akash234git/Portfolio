8fa2f6d0e10485f9f04cd0e579319fb32f9efdd4
akash-23git <uic.17bca1245@gmail.com>
2021-07-19 10:10:35 +0530

    Completed portfolio page

---
+++ src/data.js
@@ -0,0 +1,190 @@
+
+
+export const featuredPortfolio = [
+    {
+      id: 1,
+      title: "Social Media App",
+      img:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&usqp=CAU",
+    },
+    {
+      id: 2,
+      title: "Rampa UI Design",
+      img:
+        "assets/image2.png",
+    },
+    {
+      id: 3,
+      title: "E-commerce Web Design",
+      img:
+        "assets/image3.png",
+    },
+    {
+      id: 4,
+      title: "Relax Mobile App",
+      img:
+        "assets/image4.png",
+    },
+    {
+      id: 5,
+      title: "Hero Web Design",
+      img:
+        "assets/image5.png",
+    },
+    {
+      id: 6,
+      title: "Banking App",
+      img:
+        "assets/image6.png",
+    },
+  ];
+
+  export const webPortfolio = [
+    {
+      id: 1,
+      title: "Web Social Media App",
+      img: "assets/imagew1.png",
+    },
+    {
+      id: 2,
+      title: "Web Rampa UI Design",
+      img: "assets/imagew2.png",
+    },
+    {
+      id: 3,
+      title: "Web E-commerce Design",
+      img: "assets/imagew3.png",
+    },
+    {
+      id: 4,
+      title: "Web Relax App",
+      img: "assets/imagew4.png",
+    },
+    {
+      id: 5,
+      title: "Web Design",
+      img: "assets/imagew5.png",
+    },
+    {
+      id: 6,
+      title: "Web Banking App",
+      img: "assets/imagew6.png",
+    },
+  ];
+
+  export const mobilePortfolio = [
+    {
+      id: 1,
+      title: "Mobile Social Media App",
+      img:
+        "assets/imagem1.png",
+    },
+    {
+      id: 2,
+      title: "Mobile Rampa UI Design",
+      img:
+        "assets/imagem2.png",
+    },
+    {
+      id: 3,
+      title: "Mobile E-commerce Design",
+      img:
+        "assets/imagem3.png",
+    },
+    {
+      id: 4,
+      title: "Mobile Relax App",
+      img:
+        "assets/imagem4.png",
+    },
+    {
+      id: 5,
+      title: "Mobile Hero Design",
+      img:
+        "assets/imagem5.png",
+    },
+    {
+      id: 6,
+      title: "Mobile Banking App",
+      img:
+        "assets/imagem6.png",
+    },
+  ];
+
+  export const designPortfolio = [
+    {
+      id: 1,
+      title: "Design Social Media App",
+      img:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&usqp=CAU",
+    },
+    {
+      id: 2,
+      title: "Design Rampa UI Design",
+      img:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&usqp=CAU",
+    },
+    {
+      id: 3,
+      title: "Design E-commerce Web Design",
+      img:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&usqp=CAU",
+    },
+    {
+      id: 4,
+      title: "Design Relax Mobile App",
+      img:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&usqp=CAU",
+    },
+    {
+      id: 5,
+      title: "Design Keser Web Design",
+      img:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&usqp=CAU",
+    },
+    {
+      id: 6,
+      title: "Design Banking App",
+      img:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&usqp=CAU",
+    },
+  ];
+
+  export const contentPortfolio = [
+    {
+      id: 1,
+      title: "Content Social Media App",
+      img:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&usqp=CAU",
+    },
+    {
+      id: 2,
+      title: "Content Rampa UI Design",
+      img:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&usqp=CAU",
+    },
+    {
+      id: 3,
+      title: "Content E-commerce Web Design",
+      img:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&usqp=CAU",
+    },
+    {
+      id: 4,
+      title: "Content Relax Mobile App",
+      img:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&usqp=CAU",
+    },
+    {
+      id: 5,
+      title: "Content Keser Web Design",
+      img:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&usqp=CAU",
+    },
+    {
+      id: 6,
+      title: "Content Banking App",
+      img:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&usqp=CAU",
+    },
+  ];
