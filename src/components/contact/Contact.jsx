2e3b9ddeaabd7562853b870e053309f3e46f1f4a
akash-23git <uic.17bca1245@gmail.com>
2021-07-21 09:10:05 +0530

    Completed contact page

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,8 +1,28 @@
-import "./contact.scss"
+import { useState } from "react";
+import "./contact.scss";
+import { ReactSVG } from "react-svg"
 
-export default function Contact(){
-  return(
-    <div className="Contact" id="contact">
+export default function Contact() {
+  const [message, setMessage] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setMessage(true);
+  };
+  return (
+    <div className="contact" id="contact">
+      <div className="left">
+        <object height="100%"data="assets/shake.svg" type="image/svg+xml"></object>
+      </div>
+      <div className="right">
+        <h2>Contact.</h2>
+        <form onSubmit={handleSubmit}>
+          <input type="text" placeholder="Email" />
+          <textarea placeholder="Message"></textarea>
+          <button type="submit">Send</button>
+          {message && <span>Thanks, I'll reply ASAP :)</span>}
+        </form>
+      </div>
     </div>
-  )
+  );
 }
--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -4,20 +4,20 @@ export default function Testimonials() {
   const data = [
     {
       id: 1,
-      name: "Tom Durden",
+      name: "Abhi Chaurasia",
       title: "Senior Developer",
       img:
-        "https://images.pexels.com/photos/1680172/pexels-photo-1680172.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSD4k26qGzvi1xcBVBWbGwXULQVOQjsocruaQ&usqp=CAU",
       icon: "assets/twitter.png",
       desc:
         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat magnam dolorem.",
     },
     {
       id: 2,
-      name: "Alex Kalinski",
-      title: "Co-Founder of DELKA",
+      name: "Alok Kr. Mishra",
+      title: "Co-Founder Stay1.in",
       img:
-        "https://images.pexels.com/photos/428321/pexels-photo-428321.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS2KXPn-mqBJmPYU8EFHUPzd1fleI3_UfBNZQ&usqp=CAU",
       icon: "assets/youtube.png",
       desc:
         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat magnam dolorem recusandae perspiciatis ducimus vel hic temporibus. ",
@@ -25,10 +25,10 @@ export default function Testimonials() {
     },
     {
       id: 3,
-      name: "Martin Harold",
-      title: "CEO of ALBI",
+      name: "Apurva Singh",
+      title: "CEO of getqube",
       img:
-        "https://images.pexels.com/photos/3863793/pexels-photo-3863793.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSzbXhRQyrUHdQ0_90K5AOchAx_ThUNbAvI5Q&usqp=CAU",
       icon: "assets/linkedin.png",
       desc:
         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat magnam dolorem",
